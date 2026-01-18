c093c5d7ca5c4aaffbd2cde197a0e0134fbb463c
Mohamed Mahmoud Hasan <engmedo.9498@gmail.com>
2026-01-18 21:37:11 +0200

    update project

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,29 @@
  * Import your modules and initialize the app here.
  */
 
+let mealsAreasContainer = document.querySelector('#meals-areas')
 
+
+
+// console.log(getMealsAreas());
+
+
+async function getMealsAreas() {
+    let response = await fetch('https://nutriplan-api.vercel.app/api/meals/areas');
+    let mealsAreas = [];
+    
+    if (response.ok) {
+        let data = await response.json();
+        for (let area of data.results) {
+            mealsAreas.push(area.name);
+            if (area.name == 'Egyptian') break;
+        }
+        return mealsAreas;
+    }
+    return [];
+}
+
+async function displayMealsAreas(){
+    const areas = await getMealsAreas();
+    
+}
